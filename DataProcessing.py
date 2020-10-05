39af7f4ae9c2c7ef187a6f08650ef806c60c440d
171250680 <1500542139@qq.com>
2020-10-06 02:11:27 +0800

    a

--- DataProcessing.py
+++ DataProcessing.py
@@ -1,25 +1,43 @@
 import xlrd
-filename = "../任务二数据.xlsx"
 
-book = xlrd.open_workbook(filename)
+#获取文件路径
+book = xlrd.open_workbook("../任务二数据.xlsx")
 
-sheet1 = book.sheet_by_name("信息类一")
-sheet2 = book.sheet_by_name("信息类二")
+#获取表
+table1 = book.sheet_by_name("信息类一")
+table2 = book.sheet_by_name("信息类二")
 
-nrows1 = sheet1.nrows
-nrows2 = sheet2.nrows
+#获取总行数总列数
+row_Num1 = table1.nrows
+col_Num1 = table1.ncols
+row_Num2 = table2.nrows
+col_Num2 = table2.ncols
 
-row_list1 = []
-row_list2 = []
+info1 = []
+info2 = []
+key1 = table1.row_values(1)#找出字典的key值
+key2 = table2.row_values(1)
 
-for i in range(2,nrows1):
-    row_data = sheet1.row_values(i)
-    row_list1.append(row_data)
-# for i in row_list1:
-#     print(i)
+j = 1
+for i in range(row_Num1-1):
+    d ={}
+    values = table1.row_values(j)
+    for x in range(col_Num1):
+        # 把key值对应的value赋值给key，每行循环
+        d[key1[x]]=values[x]
+    j+=1
+    # 把字典加到列表中
+    info1.append(d)
+info1.pop(0)
 
-for i in range(2,nrows2):
-    row_data = sheet2.row_values(i)
-    row_list2.append(row_data)
-# for i in row_list2:
-#     print(i)
+k = 1
+for i in range(row_Num2-1):
+    d ={}
+    values = table2.row_values(k)
+    for x in range(col_Num2):
+        # 把key值对应的value赋值给key，每行循环
+        d[key2[x]]=values[x]
+    k+=1
+    # 把字典加到列表中
+    info2.append(d)
+info2.pop(0)
